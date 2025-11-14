8bbe983f6b383c590f935d025eef582d5a2a69b5
Ayushganeshi <ayushlatehar@gmail.com>
2025-11-14 13:17:40 +0530

    Add files via upload

---
+++ repetition.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t=1;
+	
+	while(t--)
+	{
+	    int ans=1;
+	    int count=1;
+	    int n;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(s[i]==s[i+1])
+	        {
+	            count++;
+	        }
+	        else
+	        {
+	            count=1;
+	        }
+	        ans=max(count,ans);
+	    }
+	    cout<<ans<<endl;
+	}
+
+}
