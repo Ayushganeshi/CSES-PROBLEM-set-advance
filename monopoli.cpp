c83505ddd71d070c56e7f756ea83bac65196c58d
Ayushganeshi <ayushlatehar@gmail.com>
2025-11-25 23:17:02 +0530

    Update monopoli.cpp

--- monopoli.cpp
+++ monopoli.cpp
@@ -14,7 +14,7 @@
 #define rall(x) (x).rbegin(), (x).rend()
 #define int long long
 
-using namespace std;
+
 using namespace __gnu_pbds;
 
 template <typename T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
