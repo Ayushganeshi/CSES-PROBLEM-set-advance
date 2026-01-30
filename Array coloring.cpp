7f69c9d05b1671870ee3ccd038e9734b3c7543b2
Ayushganeshi <ayushlatehar@gmail.com>
2026-01-30 17:04:55 +0530

    Create Array coloring.cpp

---
+++ Array coloring.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin >> i;
+    }
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        cnt += (a[i] % 2 != (i + 1) % 2);
+    }
+
+    cout << (cnt == 0 || cnt == n? "YES" : "NO") << '\n';
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int ttt = 1;
+    cin >> ttt;
+    while(ttt--) {
+        solve();
+    }
+}
