eea06789ea55975b852dd9b5591a569845069731
Ayushganeshi <ayushlatehar@gmail.com>
2026-04-03 10:58:15 +0530

    Create K closes elemnt .cpp

---
+++ K closes elemnt .cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>>pq;
+        for(auto ele: arr)
+        {
+            int dist=abs(x-ele);
+            
+            pq.push({dist,ele});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+            
+        }
+        vector<int>ans;
+
+
+        while(pq.size()>0)
+        {
+            
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
