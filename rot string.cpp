2109906f5e4218c2f9813640c348b1e67cac9dad
Ayushganeshi <ayushlatehar@gmail.com>
2026-05-03 17:28:30 +0530

    Update rot string.cpp

--- rot string.cpp
+++ rot string.cpp
@@ -3,8 +3,8 @@ public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
 
-        string doubled = s + s;///////////////// log n
+        string doubled = s + s;/////////  //// time coplexity 0(n)
 
-        return doubled.find(goal) != string::npos;////////////// o(n)
+        return doubled.find(goal) != string::npos;////////////// space complexity   0(1)
     }
 };
